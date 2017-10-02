7dfd05fc0ff3b7157792b14bcb6f108e36ff5f13
白宇东 <summer88123@163.com>
2017-10-02 20:30:16 +0800

    首页更新为bottomNav

--- app/src/main/java/com/summer/bnade/home/MainActivity.java
+++ app/src/main/java/com/summer/bnade/home/MainActivity.java
@@ -1,24 +1,17 @@
 package com.summer.bnade.home;
 
-import com.google.android.gms.ads.AdRequest;
-import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.inputmethod.InputMethodManager;
 
 import com.summer.bnade.R;
 import com.summer.bnade.base.BaseActivity;
@@ -30,18 +23,14 @@ import com.summer.bnade.token.WowTokenFragment;
 import butterknife.BindView;
 
 public class MainActivity extends BaseActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @BindView(R.id.toolbar)
     Toolbar toolbar;
     @BindView(R.id.fab)
     FloatingActionButton fab;
-    @BindView(R.id.drawer_layout)
-    DrawerLayout drawer;
     @BindView(R.id.nav_view)
-    NavigationView navigationView;
-    @BindView(R.id.adView)
-    AdView mAdView;
+    BottomNavigationView navigationView;
 
     FragmentManager fm;
 
@@ -69,26 +58,10 @@ public class MainActivity extends BaseActivity
             }
         });
 
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
 
-        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setOnNavigationItemSelectedListener(this);
 
         selectSearch();
-
-        AdRequest adRequest = new AdRequest.Builder().addTestDevice("408A5D0DD4D6B0C21C92D6D79C3E2388").build();
-        mAdView.loadAd(adRequest);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -138,28 +111,17 @@ public class MainActivity extends BaseActivity
             fm.beginTransaction()
                     .setCustomAnimations(R.anim.fragment_slide_left_enter, R.anim.fragment_slide_left_exit)
                     .replace(R.id.content_main, player, PlayerItemFragment.TAG).commit();
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         setTitle(item.getTitle());
-        drawer.closeDrawer(GravityCompat.START);
-        closeSoftInput();
         return true;
-    }
-
-    private void closeSoftInput() {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(drawer.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
     }
 
     private void selectSearch() {
         SearchFragment search = SearchFragment.getInstance(fm);
         fm.beginTransaction().setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
                 .replace(R.id.content_main, search, SearchFragment.TAG).commit();
-        navigationView.setCheckedItem(R.id.btn_item_search);
+        navigationView.setSelectedItemId(R.id.btn_item_search);
         setTitle(R.string.item_search);
     }
 
